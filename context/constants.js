ebeafeb645c7b3a623d5ce82d0c66d8273269082
zedparman <58415963+zedparman@users.noreply.github.com>
2024-02-05 14:45:02 +0330

    Update constants.js

--- context/constants.js
+++ context/constants.js
@@ -2,5 +2,6 @@
 
 import funToken from "./FunToken.json";
 
+// export const funTokenAddress = "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266";
 export const funTokenAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 export const funTokenABI = funToken.abi;
